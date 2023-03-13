e2c0b38f1675c6f38da6eaae49d95acceba9f3c5
nealwp <mrprestonneal@gmail.com>
2023-03-12 23:06:45 -0700

    update option array to have right answer at right index

--- src/components/GuessButtons.vue
+++ src/components/GuessButtons.vue
@@ -1,20 +1,31 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { answer } from '../services/addition.service';
 
-const generateWrongAnswers = () => {
-    const wrongAnswers: number[] = []
-    while (wrongAnswers.length < 4){
-        const wrongAnswer = Math.floor(Math.random() * 18)
-        if (wrongAnswer !== answer && !wrongAnswers.includes(wrongAnswer)){
-            wrongAnswers.push(wrongAnswer)
+let clicked = ref();
+const correctIndex = Math.floor(Math.random() * 4)
+
+const generateResponseOptions = () => {
+    
+    const responses: number[] = [];
+
+    while (responses.length < 4) {
+        const response = Math.floor(Math.random() * 18)
+        if (response !== answer && !responses.includes(response)){
+            responses.push(response)
         }
     }
-    return wrongAnswers
+
+    responses[correctIndex] = answer
+
+    return responses
 }
 
-const wrongAnswers = generateWrongAnswers()
+const responseOptions = generateResponseOptions()
 
-const correctAnswerButtonIndex = Math.floor(Math.random() * 4)
+const answerButtonClicked = (response: number) => {
+    clicked.value = response;
+}
 
 </script>
 
@@ -23,8 +34,9 @@ const correctAnswerButtonIndex = Math.floor(Math.random() * 4)
         <button
             v-for="n in 4"
             type="button" 
-            class="border-2 p-10 mx-2 text-3xl rounded-lg hover:bg-teal-100 shadow-md">
-            {{ n - 1 == correctAnswerButtonIndex ? answer : wrongAnswers[n - 1] }}
+            class="border-2 p-10 mx-2 text-3xl rounded-lg hover:bg-teal-100 shadow-md"
+            @click="answerButtonClicked(responseOptions[n-1])">
+            {{ responseOptions[n - 1] }}
         </button>
     </div>
 </template>
