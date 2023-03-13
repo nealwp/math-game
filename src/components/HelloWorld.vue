e2ffb5c45d3ff434e89b3d0f8cbac78dd61fa68b
nealwp <mrprestonneal@gmail.com>
2023-03-12 19:18:29 -0700

    add tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -28,7 +28,7 @@ const count = ref(0)
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
     in your IDE for a better DX
   </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p class="text-xl">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
