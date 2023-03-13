bd17b6727541c75e1304b93564fa65eba4432ee2
nealwp <mrprestonneal@gmail.com>
2023-03-12 19:43:39 -0700

    add topbar

---
+++ src/components/TopBar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="bg-white border-b-2 py-4 flex shadow-sm">
+    <span class="mx-auto font-bold">App Title</span>
+  </div>
+
+</template>
+
+<style scoped></style>
