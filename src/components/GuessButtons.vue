d18f0b5ab6cd07c4bc6285add084a87c0734c119
nealwp <mrprestonneal@gmail.com>
2023-03-12 22:03:49 -0700

    answer is generating and writing in random button

--- src/components/GuessButtons.vue
+++ src/components/GuessButtons.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
+import { answer } from '../services/addition.service';
+
+const correctAnswerButton = Math.floor(Math.random() * 4)
 
 </script>
 
 <template>
     <div class="flex justify-center">
-            <button type="button" class="border-2 p-10 mx-2 text-3xl rounded-lg hover:bg-teal-100 shadow-md">0</button>
-            <button type="button" class="border-2 p-10 mx-2 text-3xl rounded-lg hover:bg-teal-100 shadow-md">0</button>
-            <button type="button" class="border-2 p-10 mx-2 text-3xl rounded-lg hover:bg-teal-100 shadow-md">0</button>
-            <button type="button" class="border-2 p-10 mx-2 text-3xl rounded-lg hover:bg-teal-100 shadow-md">0</button>
+        <button
+            v-for="n in 4"
+            type="button" 
+            class="border-2 p-10 mx-2 text-3xl rounded-lg hover:bg-teal-100 shadow-md">
+            {{ n - 1 == correctAnswerButton ? answer : 0 }}
+        </button>
     </div>
 </template>
 
